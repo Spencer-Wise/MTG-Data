050503dc855ce114def528c6706a4b61b053ee4f
Spencer Wise <swise52@gmail.com>
2021-01-05 16:19:29 -0700

    Added a check for staleness of the MTGO data (and updated data)

--- MTG_Data_Analysis.py
+++ MTG_Data_Analysis.py
@@ -2,9 +2,16 @@
 
 import json
 import statistics as stats
+from datetime import datetime
+from datetime import date
 
 import Cardhoarder_Scraper
 import Scryfall_API
+
+print('\n')
+
+today = datetime.now().date()
+# randomdate = date(2020, 1, 5)
 
 file1 = open('MTGPriceData.json')
 data = json.load(file1)
@@ -29,21 +36,24 @@ for card, dates in data.items():
         sellcans[card] = zscore
     if zscore < -1.96:
         buycans[card] = zscore
-    # print(prices, avg, std, zscore)
 
 print('Scryfall data:')
 print(f'Sell candidates- {sellcans}')
+print('\n')
 print(f'Buy candidates- {buycans}')
 
-file1 = open('MTGOCollectionPrices.json')
-data2 = json.load(file1)
-file1.close()
+file2 = open('MTGOCollectionPrices.json')
+data2 = json.load(file2)
+file2.close()
 
 sellcans = {}
 buycans = {}
 
 for card, dates in data2.items():
-    pricelist = [price for price in dates.values()]
+    datelist, pricelist = [date for date in dates.keys()], [price for price in dates.values()]
+    lastdate = datelist[-1].split('-')
+    lastdateformatted = date(int(lastdate[-1]), int(lastdate[0]), int(lastdate[1]))
+    delta = today - lastdateformatted
     prices = [float(price) for price in pricelist]
     prices = prices[-x:]
     avg = stats.mean(prices)
@@ -55,12 +65,13 @@ for card, dates in data2.items():
         zscore = (prices[-1] - avg) / std
     except ZeroDivisionError:
         zscore = 0
-    if zscore > 1.96:
+    if zscore > 1.96 and delta.days < 10:
         sellcans[card] = zscore
-    if zscore < -1.96:
+    if zscore < -1.96 and delta.days < 10:
         buycans[card] = zscore
 
 print('\n')
 print('Cardhoarder data:')
 print(f'Sell candidates- {sellcans}')
+print('\n')
 print(f'Buy candidates- {buycans}')
